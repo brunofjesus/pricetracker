771050ab3fc0ced3be1ea5e3006cc2055d98aa84
Bruno Jesus <bruno.fl.jesus@gmail.com>
2023-09-15 00:59:45 +0100

    Initial commit

---
+++ store/definition.go
@@ -0,0 +1,22 @@
+package store
+
+import "github.com/shopspring/decimal"
+
+type Store interface {
+	Id() string
+	Name() string
+	Website() string
+	Crawl(productChannel chan StoreProduct)
+}
+
+type StoreProduct struct {
+	StoreId   string
+	StoreName string
+	EAN       []string
+	SKU       []string
+	Name      string
+	Brand     string
+	Price     decimal.Decimal
+	Available bool
+	Link      string
+}
