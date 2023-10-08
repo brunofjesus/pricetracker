0aaeefc5b53d2f5e404cb19fdbbb34468855e7f1
Bruno Jesus <bruno.fl.jesus@gmail.com>
2023-10-08 22:50:28 +0100

    Worten ignore products without url

--- stores/worten/integration/store/producthandler.go
+++ stores/worten/integration/store/producthandler.go
@@ -1,7 +1,9 @@
 package store
 
 import (
+	"errors"
 	"log/slog"
+	"strings"
 
 	"github.com/brunofjesus/pricetracker/stores/worten/definition/catalog"
 	"github.com/brunofjesus/pricetracker/stores/worten/definition/store"
@@ -15,6 +17,11 @@ type ProductHandler struct {
 }
 
 func (h *ProductHandler) Handle(wph store.WortenProductHit) error {
+	if len(strings.TrimSpace(wph.Product.URL)) == 0 {
+		h.Logger.Error("Product has no url", slog.String("service", "mapper"), slog.Any("worten_product", wph))
+		return errors.New("product has no url")
+	}
+
 	var storeProduct catalog.StoreProduct = catalog.StoreProduct{}
 	err := mapping.MapWortenProductToCatalogProduct(wph, &storeProduct)
 	if err != nil {
@@ -24,7 +31,7 @@ func (h *ProductHandler) Handle(wph store.WortenProductHit) error {
 
 	err = h.Publisher.PublishProduct(storeProduct)
 	if err != nil {
-		h.Logger.Error("Error publishing product", slog.Any("error", err))
+		h.Logger.Error("Error publishing product", slog.String("service", "mapper"), slog.Any("error", err))
 	}
 
 	return err
