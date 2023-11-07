aa566bc97432bb8987b6220e0d4f5eb20ebea06c
Bruno Jesus <bruno.fl.jesus@gmail.com>
2023-11-07 00:44:59 +0000

    Make view match struct

--- catalog/migration/000006_create_metrics_view.up.sql
+++ catalog/migration/000006_create_metrics_view.up.sql
@@ -1,14 +1,22 @@
 CREATE VIEW product_metrics
-            (product_id, price, discount_percent, diff, average, maximum, minimum, entries, metrics_since) as
+            (product_id, store_id, name, brand, price, available, image_url, product_url, discount_percent, diff,
+             average, maximum, minimum, entries, metrics_since)
+as
 SELECT p.product_id,
+       p.store_id,
+       p.name,
+       p.brand,
        p.price,
+       p.available,
+       p.image_url,
+       p.product_url,
        (avg(pp.price) - p.price) / avg(pp.price) AS discount_percent,
-       p.price - avg(pp.price)                  AS diff,
-       avg(pp.price)                            AS average,
-       max(pp.price)                            AS maximum,
-       min(pp.price)                            AS minimum,
-       count(pp.price)                          AS entries,
-       now() - '30 days'::interval              AS metrics_since
+       p.price - avg(pp.price)                   AS diff,
+       avg(pp.price)                             AS average,
+       max(pp.price)                             AS maximum,
+       min(pp.price)                             AS minimum,
+       count(pp.price)                           AS entries,
+       now() - '30 days'::interval               AS metrics_since
 FROM product_price pp
          JOIN product p ON p.product_id = pp.product_id
 WHERE pp.date_time > (now() - '30 days'::interval)
