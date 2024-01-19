7cd0f61ec21fe3c6efe8479da97b7fcd493b7ebb
Bruno Jesus <bruno.fl.jesus@gmail.com>
2024-01-19 19:26:20 +0000

    Cleanup CSS and JS dependencies

---
+++ catalog/pkg/http/frontend/static/js/handlebars-helpers.js
@@ -0,0 +1,27 @@
+Handlebars.registerHelper( "when",function(operand_1, operator, operand_2, options) {
+    var operators = {
+        'eq': function(l,r) { return l == r; },
+        'noteq': function(l,r) { return l != r; },
+        'gt': function(l,r) { return Number(l) > Number(r); },
+        'or': function(l,r) { return l || r; },
+        'and': function(l,r) { return l && r; },
+        '%': function(l,r) { return (l % r) === 0; }
+    }
+        , result = operators[operator](operand_1,operand_2);
+
+    if (result) return options.fn(this);
+    else  return options.inverse(this);
+});
+
+Handlebars.registerHelper("math", function(lvalue, operator, rvalue, options) {
+    lvalue = parseFloat(lvalue);
+    rvalue = parseFloat(rvalue);
+
+    return {
+        "+": lvalue + rvalue,
+        "-": lvalue - rvalue,
+        "*": lvalue * rvalue,
+        "/": lvalue / rvalue,
+        "%": lvalue % rvalue
+    }[operator];
+});
